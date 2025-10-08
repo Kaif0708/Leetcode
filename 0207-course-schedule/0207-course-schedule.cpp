fa7cfbc1606435ea3d31b6af1cb46fb10e4af71e
Kaif0708 <110331551+Kaif0708@users.noreply.github.com>
2025-10-08 10:07:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (62.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
+    bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis,vector<int>&pathVis){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(it,adj,vis,pathVis)) return true;
+            } else if(pathVis[it]) return true;//cycle
+        }
+        pathVis[node]=0;
+        return false;
+        
+    }
     bool canFinish(int V, vector<vector<int>>& prerequisites) {
-    vector<vector<int>> adj(V);     
-    vector<int> indegree(V, 0);     
-
-    for (auto& pre : prerequisites) {
+    //using topo+bfs
+    // vector<vector<int>> adj(V);
+    // vector<int> indegree(V,0);
+    // for(auto& pre: prerequisites){
+    //     adj[pre[1]].push_back(pre[0]);
+    //     indegree[pre[0]]++;
+    // }
+    // queue<int>q;
+    // for(int i=0;i<V;i++){
+    //     if(indegree[i]==0) q.push(i);
+    // }
+    // int cnt=0;
+    // while(!q.empty()){
+    //     int node=q.front();
+    //     q.pop();
+    //     cnt++;
+    //     for(int neigh:adj[node]){
+    //         indegree[neigh]--;
+    //         if(indegree[neigh]==0) q.push(neigh);
+    //     }
+    // }
+    // return cnt==V;
+    
+    vector<int>vis(V,0),pathVis(V,0);
+    vector<vector<int>>adj(V);
+    for(auto &pre:prerequisites){
         adj[pre[1]].push_back(pre[0]);
-        indegree[pre[0]]++;
     }
-
-    queue<int> q;
-    for (int i = 0; i < V; i++) {
-        if (indegree[i] == 0) q.push(i);
-    }
-
-    int count = 0;
-    while (!q.empty()) {
-        int node = q.front();
-        q.pop();
-        count++;
-
-        for (int neigh : adj[node]) {
-            indegree[neigh]--;
-            if (indegree[neigh] == 0) q.push(neigh);
+    
+    
+    for(int i=0;i<V;i++){
+        if(!vis[i]){
+            if(dfs(i,adj,vis,pathVis)) return false;
         }
     }
+    return true;
 
-    return count == V;
 }
 
 };
